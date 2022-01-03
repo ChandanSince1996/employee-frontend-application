22da2fdec883d8099767a626b8940fc58ec77218
Chandan Rai <nckr2014@gmail.com>
2022-01-04 02:33:41 +0530

    initial setup and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
+import { EmployeeAllComponent } from './employee-all/employee-all.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegisterComponent,
+    EmployeeAllComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
